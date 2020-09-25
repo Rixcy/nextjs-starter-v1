6bd59d8414f89cbd439815b1caaa45a029c178cd
Rixcy <admin@rickbooth.me>
2020-09-25 23:04:07 +0100

    feat: add site icon

---
+++ pages/_document.tsx
@@ -0,0 +1,42 @@
+import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+    static async getInitialProps(ctx: DocumentContext) {
+        const initialProps = await Document.getInitialProps(ctx)
+
+        return initialProps
+    }
+
+    render() {
+        return (
+            <Html lang="en">
+                <Head>
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="180x180"
+                        href="icons/apple-touch-icon.png"
+                    />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="32x32"
+                        href="icons/favicon-32x32.png"
+                    />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="16x16"
+                        href="icons/favicon-16x16.png"
+                    />
+                    <link rel="manifest" href="icons/site.webmanifest" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
+
+export default MyDocument
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,6 @@ export default function Home() {
         <div className={styles.container}>
             <Head>
                 <title>Create Next App</title>
-                <link rel="icon" href="/favicon.ico" />
             </Head>
 
             <main className={styles.main}>
@@ -46,16 +45,6 @@ export default function Home() {
                     </a>
                 </div>
             </main>
-
-            <footer className={styles.footer}>
-                <a
-                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Powered by <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-                </a>
-            </footer>
         </div>
     )
 }
